8e2ec6ae30937b2f2fe9374706cb0fc775fb34e9
Dachi301 <qirikashvili2020@gmail.com>
2025-01-04 20:32:50 +0400

    fixed small issue

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,14 @@
+'use client'
+
 import React from "react";
 
 export default function Footer() {
+    const year = new Date().getFullYear()
+
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2024 Dachi. All rights reserved.
+        &copy; {year} Dachi. All rights reserved.
       </small>
     </footer>
   );
